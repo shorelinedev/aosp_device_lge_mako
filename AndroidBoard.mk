09d8c3f1977e62dc8c2894d040aba72147b14b3d
Matthew <m.hanhimaki@gmail.com>
2014-10-27 16:34:54 -0400

    disable inline for prebuilt testing on mako kernel

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -13,15 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-KERNEL_DEFCONFIG := geeb_defconfig
-KERNEL_DIR := kernel/geeb
-
--include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
-
+#
+#KERNEL_DEFCONFIG := geeb_defconfig
+#KERNEL_DIR := kernel/geeb
+#
+#-include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+#
 # device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
 # So cp will do.
-.PHONY: $(PRODUCT_OUT)/kernel
-$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
-	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+#.PHONY: $(PRODUCT_OUT)/kernel
+#$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+#	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
 	
